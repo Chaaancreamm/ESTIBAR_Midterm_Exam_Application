67306b02e218b9cb6c30d3dca1ba917c711f3cbe
Chaaancreamm <cce7990@students.uc-bcf.edu.ph>
2024-02-21 09:34:05 +0800

    Update: Created Controller and defined products

---
+++ ESTIBAR_Midterm_Exam_Application/resources/views/movies.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movies</title>
+</head>
+<body>
+    <div class="movies-container">
+        <h1>MOVIES</h1>
+        <ul>
+            <li>
+                The Lord of The Rings: Fellowship of the Ring
+            </li>
+            <li>
+                The Lord of The Rings: The Two Towers
+            </li>
+            <li>
+                The Lord of The Rings: Return Of The King
+            </li>
+            <li>
+                El Camino
+            </li>
+            <li>
+                Star Wars: Revenge of the Sith
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
